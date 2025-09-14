527599e5d7cb7188b06b342f4244df56ddb1cd5d
Valtriaz <valtriaz@gmail.com>
2025-09-14 11:59:54 +0530

    11-String.c Completed

---
+++ CSI_Grassroot/11-String.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[20] = "Grassroots";
+    int length;
+    length = strlen(str);
+    printf("The string is : %d", str);
+    printf("Length = %d\n", length);
+    return 0;
+}
